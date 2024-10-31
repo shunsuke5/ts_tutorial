cd403b0b91773ec3d4db17f82942e774a9e1a1ff
shunsuke5 <shunsuke1359@gmail.com>
2024-10-31 18:48:03 +0900

    オプショナルチェーン 完了

--- read-study-ts/Object.ts
+++ read-study-ts/Object.ts
@@ -152,3 +152,241 @@ console.log("\nオブジェクトの型のreadonlyプロパティ");
         f: number;
     }>;
 }
+
+console.log("\nreadonlyとconstの違い");
+{
+    const x = 1;
+    // x = 2;   // エラー
+    const x1 = { y: 1 };
+    x1.y = 2;   // プロパティへの代入はOK
+
+    let obj: { readonly x: number } = { x : 1 };
+    // obj.x = 2;  // エラー
+    obj = { x: 2 }; // 変数自体への代入はOK
+
+    const obj1: { readonly x: number } = { x: 1 };
+    // obj1 = { x: 2 };    // エラー
+    // obj1.x = 2;         // エラー
+}
+
+console.log("\nオブジェクトの型のオプションプロパティ");
+{
+    type Size = {
+        width?: number;
+    };
+    const size: Size = {};  // OK
+    const size1: Size = {
+        width: undefined,   // OK
+    };
+    console.log(size + "\t" + size1);
+
+    const size2: Size = {
+        // width: null,    // エラー
+    };
+}
+
+console.log("\n余剰プロパティチェック");
+{
+    let onlyX: { x: number };
+    onlyX = { x: 1 };   // OK
+    // onlyX = { x:1, y:2 };   // コンパイルエラー
+
+    const xy: { x: number; y: number } = { x: 1, y: 2};
+    onlyX = xy;    // 変数代入にはチェックが働かないのでOK
+    console.log(onlyX);
+}
+
+console.log("\nインデックス型");
+{
+    let obj: {
+        [K: string]: number;    // K は型変数名で、フィールド名の型はstring,number,symbolのみ指定可能
+    };
+    obj = { a: 1, b: 2 };   // OK
+    obj.c = 4;              // OK
+    obj["d"] = 5;           // OK
+    console.log(obj);
+
+    const e: number | undefined = obj.e;
+    console.log(e); // undefined
+
+    let obj1: { [K: string]: number };
+    let obj2: Record<string, number>;
+}
+
+console.log("\nプロトタイプベース");
+{
+    const button = {
+        name: "ボタン",
+    };
+    const dangerousButton = Object.create(button);
+    dangerousButton.name = "絶対に押すなよ？";
+    console.log(dangerousButton.name);
+
+    const counter = {
+        count: 0,
+        countUp() {
+            this.count++;
+        },
+    };
+    const resettableCounter = Object.create(counter);
+    resettableCounter.reset = function () {
+        this.count = 0;
+    };
+    resettableCounter.countUp();
+    console.log(resettableCounter.count);   // 1
+    resettableCounter.reset();
+    console.log(resettableCounter.count);   // 0
+
+    // ES2015からはクラスのような書き方もできる
+    class Counter {
+        count: 0;
+        constructor() {
+            this.count = 0;
+        }
+        countUp() {
+            this.count++;
+        }
+    }
+    let counter1 = new Counter();
+    console.log(counter1.count);
+    counter1.countUp();
+    console.log(counter1.count);
+}
+
+console.log("\nobject,Object,{}の違い");
+{
+    let obj: { a: number; b: number };
+    let a: object = {}; // OK
+    let b: Object = {}; // OK
+    let c: {} = {};     // OK
+    console.log(a + "\t" + b + "\t" + c);
+
+    console.log("object型");
+    {
+        let a: object;
+        a = { x: 1 };   // OK
+        a = [1, 2, 3];  // OK
+        a = /a-z/;      // OK
+        // a = 1;          // エラー
+        // a = true;       // エラー
+        // a = "string";   // エラー
+    }
+    console.log("Object型");
+    {
+        let a; Object;
+        a = {};
+        // ボックス化可能なプリミティブ型はOK
+        a = 1;
+        a = true;
+        a = "string";
+        // nullとundefinedはNG(ts2.0からは代入できる？)
+        a = null;
+        a = undefined;
+    }
+    console.log("{}型");
+    {
+        let a: {};
+        a = {};
+        // ボックス化可能なプリミティブ型OK
+        a = 1;
+        a = true;
+        a = "string"
+        // nullとundefinedはNG
+        // a = null;
+        // a = undefined;
+    }
+}
+
+console.log("\nオブジェクトの分割代入");
+{
+    const item = { price: 100 };
+    const price1 = item.price;
+    const { price } = item;
+    console.log(price1 + "\t" + price);
+
+    const obj = { a: 1, b: 2 };
+    const { a, b } = obj;
+    // 取り出すプロパティ数が多い場合に便利
+    const color = { R: 0, G: 122, B: 204, A: 1 };
+    const { R, G, B, A } = color;
+    console.log(color);
+    console.log(R + " " + G + " " + B + " " + A);
+    // コロンの後に変数名を指定すると、その名前の変数に代入できる
+    const { R: red, G: green, B: blue, A: alpha } = color;
+    console.log(green);
+
+    const continent = {
+        name: "北アメリカ",
+        us: {
+            name: "アメリカ合衆国",
+            capitalCity: "ワシントンD.C",
+        },
+    };
+    const {
+        us: {name, capitalCity },
+    } = continent;
+    console.log(name);
+    console.log(capitalCity);
+
+    console.log("分割代入のデフォルト値");
+    {
+        const color = { r: undefined, g: 122, b: 204 };
+        const { r = 0, g = 0, b = 0 } = color;
+        console.log(r, g, b);   // 0 122 204
+
+        const color1 = { r: null };
+        const { r: r1 = 0 } = color;
+        console.log(r1);    // 0
+    }
+    console.log("デフォルト値と変数名の指定");
+    {
+        const color = { r: undefined, g: 122, b: 204 };
+        const { r: red = 0 } = color;
+        console.log(red);
+    }
+}
+
+console.log("\nShorthand property names");
+{
+    type Wild = {
+        name: string;
+        no: number;
+        genre: string;
+        height: number;
+        weight: number;
+    };
+    const name = "pikachu";
+    const no = 25;
+    const genre = "mouse pokemon";
+    const height = 0.4;
+    const weight = 6.0;
+
+    const pikachu: Wild = {
+        name,
+        no,
+        genre,
+        height,
+        weight,
+    };
+}
+
+console.log("\nオプショナルチェーン");
+{
+    interface Book {
+        title: string;
+    }
+    const book: Book | undefined = undefined;
+    // const title = book.title;   // エラー
+    // const author = null;
+    // const email = author.email; // エラー
+
+    // const title1 = book?.title; // そもそもオプショナルチェーン以前にtitleなんてプロパティ無いよとエラーになる
+    // console.log(title1);
+    // オプショナルチェーンはJavaScriptの機能で、
+    // TypeScriptはそれ以前にnever型はプロパティ持てないと教えてくれているだけだった
+}
+
+console.log("\nオブジェクトをループする方法");
+{
+
+}
